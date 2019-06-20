d65d75458bafc436b49299ed4cb0c21d1fc09f9f
shreyas <shreyasanother@gmail.com>
2019-06-20 23:13:15 +0530

    second commit

--- app/src/main/java/com/example/android/aninterface/MainActivity.java
+++ app/src/main/java/com/example/android/aninterface/MainActivity.java
@@ -4,20 +4,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button button;
+    TextView tv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button.findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        tv = findViewById(R.id.tv);
+
+
+
+          tv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+             tv.animate().rotation(60).setDuration(0);
 
             }
         });
-    }
+
+
+
+
+}
 }
